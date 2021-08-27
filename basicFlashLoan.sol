81de37d6eec122209ef673d9a15e4b3a0aba4989
Dustin Gearhart <gearhart26@gmail.com>
2021-08-26 18:11:36 -0700

    Update basicFlashLoan.sol

--- basicFlashLoan.sol
+++ basicFlashLoan.sol
@@ -1,2 +1,9 @@
 //  TOPIC: DeFi 201    -->    VIDEO: "Program a Basic Flash Loan part 2"   
 
+pragma solidity ^0.5.0;
+
+import "https://github.com/mrdavey/ez-flashloan/blob/remix/contracts/aave/FlashLoanReceiverBase.sol";
+import "https://github.com/mrdavey/ez-flashloan/blob/remix/contracts/aave/ILendingPool.sol";
+import "https://github.com/mrdavey/ez-flashloan/blob/remix/contracts/aave/ILendingPoolAddressesProvider.sol";
+
+
